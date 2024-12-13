37371f5fddd1fc13359c5d8fe1cfbb68ce34def3
YamidTR <yltorresr@gmail.com>
2024-12-13 11:24:15 -0500

    Add files via upload

---
+++ yamid.js
@@ -0,0 +1,5 @@
+// scripts.js
+document.addEventListener('DOMContentLoaded', () => {
+    console.log('Document loaded');
+    // Aquí se puede agregar más funcionalidad interactiva si es necesario
+});
